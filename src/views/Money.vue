5d7c457be8be93d4fdd30de0441cb6e7e9842a43
liming <759297569@qq.com>
2021-09-12 14:57:10 +0800

    Money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    Money.vue
-    <Nav />
+  <div class="nav-wrapper">
+    <div class="content">
+      Money.vue
+    </div>
+    <Nav/>
   </div>
 </template>
 
@@ -11,6 +13,15 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.nav-wrapper {
+  display: flex;
+  border: 1px solid green;
+  flex-direction: column;
+  height: 100vh;
+}
+.content{
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
