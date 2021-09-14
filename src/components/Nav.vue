b99c62d02174e9b0e88f80eba3e4b3031a923e83
liming <759297569@qq.com>
2021-09-14 10:37:24 +0800

    增加了全局变量，修改字体，颜色

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,16 @@
 <template>
   <nav>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="标签"/>
       标签
     </router-link>
 
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="记账本"/>
       记账
     </router-link>
 
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="统计"/>
       统计
     </router-link>
@@ -24,6 +24,7 @@
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
 nav {
   display: flex;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
@@ -36,13 +37,14 @@ nav {
     justify-content: center;
     align-items: center;
     flex-direction: column;
+    color: gray;
     .icon {
       width: 32px;
       height: 32px;
     }
   }
   > .item.selected{
-    color: red;
+    color: $color-highlight
   }
 }
 </style>
