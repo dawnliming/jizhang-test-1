4f4303939a925ebd159be7170379262fc6561755
liming <759297569@qq.com>
2021-09-17 10:26:19 +0800

    添加 createdAd 字段

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,5 @@
 <template>
   <Layout class-prefix="layout">
-    {{ record }}
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
@@ -21,13 +20,14 @@ type Record = {
   notes: string
   type: string
   amount: number
+  createAt?: Date
 }
 @Component({
   components: {Notes, NumberPad, Tags, Types},
 })
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行'];
-  recordList: Record[] = [];
+  recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
   record: Record = {tags: [], notes: '', type: '-', amount: 0};
 
   onUpdateTags(value: string[]) {
@@ -39,7 +39,8 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    const record2 = JSON.parse(JSON.stringify(this.record))
+    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    record2.createAt = new Date();
     this.recordList.push(record2);
   }
 
