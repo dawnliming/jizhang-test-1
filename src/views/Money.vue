aae3710992db8625b6eebd9a440064c2a4effdec
liming <759297569@qq.com>
2021-09-18 16:26:45 +0800

    完成 CSS ，添加 EditLabel.vue 的样式，抽离了两个组件 Button 和 FormItem

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,9 +2,11 @@
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
-    <Notes field-name="备注"
-           placeholder="在这里输入备注"
-           @update:value="onUpdateNotes"/>
+    <div class="notes">
+      <FormItem field-name="备注"
+                placeholder="在这里输入备注"
+                @update:value="onUpdateNotes"/>
+    </div>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
 </template>
@@ -14,7 +16,7 @@ import Vue from "vue";
 import Types from "@/components/Money/Types.vue";
 import Tags from "@/components/Money/Tags.vue";
 import NumberPad from "@/components/Money/NumberPad.vue";
-import Notes from "@/components/Money/Notes.vue";
+import FormItem from "@/components/Money/FormItem.vue";
 import {Component, Watch} from "vue-property-decorator";
 import recordListModel from "@/models/recordListModel";
 import tagListModel from "@/models/tagListModel";
@@ -23,7 +25,7 @@ const recordList = recordListModel.fetch();
 const tagList = tagListModel.fetch();
 
 @Component({
-  components: {Notes, NumberPad, Tags, Types},
+  components: { FormItem, NumberPad, Tags, Types},
 })
 export default class Money extends Vue {
   tags = tagList;
@@ -55,5 +57,8 @@ export default class Money extends Vue {
 .layout-content {
   display: flex;
   flex-direction: column-reverse;
+}
+.notes{
+  padding: 12px 0;
 }
 </style>
