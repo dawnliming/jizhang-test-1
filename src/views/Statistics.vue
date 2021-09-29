3577cefded9f5e85e65526cfb3d85b7991cc401d
liming <759297569@qq.com>
2021-09-29 12:25:05 +0800

    完成统计页面

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,20 +1,19 @@
 <template>
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
-    <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
-      <ol>
-        <li v-for="(group,index) in result" :key="index">
-          <h3 class="title">{{group.title}}</h3>
-          <ol>
-            <li v-for="item in group.items" :key="item.id"
-            class="record">
-              <span>{{tagString(item.tags)}}</span>
-              <span class="note">{{item.notes}}</span>
-              <span>￥{{ item.amount }}</span>
-            </li>
-          </ol>
-        </li>
-      </ol>
+    <ol>
+      <li v-for="(group,index) in groupedList" :key="index">
+        <h3 class="title">{{ beautify(group.title) }} <span>￥{{ group.total }}</span></h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id"
+              class="record">
+            <span>{{ tagString(item.tags) }}</span>
+            <span class="note">{{ item.notes }}</span>
+            <span>￥{{ item.amount }}</span>
+          </li>
+        </ol>
+      </li>
+    </ol>
   </Layout>
 </template>
 
@@ -22,8 +21,9 @@
 import Vue from "vue";
 import {Component} from "vue-property-decorator";
 import Tabs from "@/components/Tabs.vue";
-import intervalList from "@/constants/intervalList";
 import recordTypeList from "@/constants/recordTypeList";
+import dayjs from "dayjs";
+import clone from "@/lib/clone";
 
 @Component({
   components: {Tabs}
@@ -31,24 +31,56 @@ import recordTypeList from "@/constants/recordTypeList";
 
 
 export default class statistics extends Vue {
-  tagString(tags: Tag[]){
-    return tags.length === 0 ? '无' : tags.join(',')
+  tagString(tags: Tag[]) {
+    return tags.length === 0 ? '无' : tags.join(',');
   }
+
+  beautify(string: string) {
+    const day = dayjs(string);
+    const now = dayjs();
+    if (day.isSame(now, 'day')) {
+      return '今天';
+    } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+      console.log('hi');
+      return '昨天';
+    } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
+      return '前天';
+    } else if (day.isSame(now, 'year')) {
+      return day.format('M月D日');
+    } else {
+      return day.format('YYYY年M月D日');
+    }
+  }
+
   get recordList() {
     return (this.$store.state as RootState).recordList;
   }
 
-  get result() {
+  get groupedList() {
     const {recordList} = this;
-    type HashTableValue = {title:string, items: RecordList[]}
-
-    const hashTable: { [key: string]: HashTableValue } = {};
-    for (let i = 0; i < recordList.length; i++) {
-      const [date, time] = recordList[i].createdAt!.split('T');
-      hashTable[date] = hashTable [date] || {title:date, items:[]};
-      hashTable[date].items.push(recordList[i]);
+    if (recordList.length === 0) {
+      return [];
     }
-    return hashTable;
+    const newList = clone(recordList)
+        .filter(r => r.type === this.type)
+        .sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf());
+    type Result = { title: string, total?: number, items: RecordItem[] }[]
+    const result: Result = [{title: dayjs(recordList[0].createdAt).format('YYYY-MM-DD'), items: [recordList[0]]}];
+    for (let i = 1; i < newList.length; i++) {
+      const current = newList[i];
+      const last = result[result.length - 1];
+      if (dayjs(last.title).isSame(dayjs(current.createdAt), 'day')) {
+        last.items.push(current);
+      } else {
+        result.push({title: dayjs(current.createdAt).format('YYYY-MM-DD'), items: [current]});
+      }
+    }
+    result.map(group => {
+      group.total = group.items.reduce((sum, item) => sum + item.amount, 0);
+      console.log(result);
+    });
+    return result;
+
   }
 
   beforeCreate() {
@@ -56,38 +88,40 @@ export default class statistics extends Vue {
   }
 
   type = '-';
-  interval = 'day';
-  intervalList = intervalList;
   recordTypeList = recordTypeList;
 }
 </script>
 
 <style scoped lang="scss">
-  %item{
-    padding: 8px 16px;
-    line-height: 24px;
-    display: flex;
-    justify-content: space-between;
-    align-content: center;
-  }
-  .title{
-    @extend %item
-  }
-  .record{
-    background: white;
-    @extend %item
-  }
-  .note{
-    margin-right: auto;
-    margin-left: 8px;
-    color: #999999;
-  }
+%item {
+  padding: 8px 16px;
+  line-height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+
+.title {
+  @extend %item
+}
+
+.record {
+  background: white;
+  @extend %item
+}
+
+.note {
+  margin-right: auto;
+  margin-left: 8px;
+  color: #999999;
+}
+
 ::v-deep {
   .type-tabs-item {
-    background: white;
+    background: #c4c4c4;
 
     &.selected {
-      background: #c4c4c4;
+      background: white;
 
       &::after {
         display: none;
